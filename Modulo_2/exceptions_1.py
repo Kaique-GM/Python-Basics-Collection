55d894051fd4b3b933579e95df7d022dc74d6442
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-14 21:05:26 -0300

    Try, Except e Finally

---
+++ Modulo_2/exceptions_1.py
@@ -0,0 +1,32 @@
+#Try
+
+try:
+    # Código que pode gerar uma exceção
+    resultado = 10 / 0  # Divisão por zero
+    print(resultado)
+except ZeroDivisionError:
+    print("Erro: Divisão por zero")
+
+
+#Except
+
+try:
+    # Código que pode gerar uma exceção
+    resultado = 10 / 0  # Divisão por zero
+    print(resultado)
+except ZeroDivisionError:
+    print("Erro: Divisão por zero")
+except ValueError:
+    print("Erro: Valor inválido")
+
+
+#Finally
+
+try:
+    # Código que pode gerar uma exceção
+    arquivo = open("arquivo.txt", "r")
+    # Realizar operações com o arquivo
+except FileNotFoundError:
+    print("Erro: Arquivo não encontrado")
+finally:
+    arquivo.close()  # Fechar o arquivo sempre, mesmo se ocorrer uma exceção
