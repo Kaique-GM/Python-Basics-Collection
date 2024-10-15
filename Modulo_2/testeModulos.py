fab64ba0c9485537947698a66c2ac48393b2395b
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-15 16:50:32 -0300

    Criação de módulos

---
+++ Modulo_2/testeModulos.py
@@ -0,0 +1,19 @@
+import meu_modulo
+import operacoes
+import utilidades
+
+
+meu_modulo.saudar("João")  # Imprime "Olá, João!"
+resultado = meu_modulo.calcular_soma(5, 3)
+print(resultado)  # Imprime 8
+
+
+print(" ")
+####
+
+resultado = operacoes.somar(5, 3)
+utilidades.imprimir_mensagem(f"O resultado da soma é: {resultado}")
+
+
+nome = utilidades.obter_nome_usuario()
+utilidades.imprimir_mensagem(f"Olá, {nome}!")
