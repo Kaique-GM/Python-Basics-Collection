99fa63e4c96ee9ef21dd5bac45062aca52738379
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-20 11:24:56 -0300

    Modulo 1++ função print()

---
+++ Modulo_1++/funcao_print.py
@@ -0,0 +1,84 @@
+#Testes da Função print()
+
+print("Olá, Python!")
+# print("Kaique")
+# print(Kaique)
+# print"Kaique"
+# print('Kaique')
+# print("Kaique") print("Python")
+
+print("A aranha pequenininha\nsubiu a tromba d'água.")
+print()
+print("abaixo veio a chuva\ne lavou a aranha.")
+
+print("A aranha pequenininha" , "subiu" , "a tromba d'água.")
+
+print("Meu nome é", "Python.", end=" ")
+print("Monty Python.")
+
+print("Meu nome é ", end="")
+print("Monty Python.")
+
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+print()
+
+print("Meu", "nome", "é", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+
+print("Programação","Essenciais","em", sep="***", end="...")
+print("Python")
+print("versão original:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("com menos 'print()' invocações:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("mais alto:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("dobrou:")
+###################
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
+
