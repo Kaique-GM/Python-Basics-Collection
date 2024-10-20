bcb108a232e9a337451e6b94450ce9c8df957560
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-20 14:00:36 -0300

    Literais

---
+++ Modulo_1++/literais.py
@@ -0,0 +1,14 @@
+print(0o123)
+
+print(0x123)
+
+print("Eu gosto \"Monty Python\"")
+
+print('Eu gosto "Monty Python"')
+
+print('I\'m Monty Python.')
+
+print(True > False)
+print(True < False)
+ 
+print("\"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
