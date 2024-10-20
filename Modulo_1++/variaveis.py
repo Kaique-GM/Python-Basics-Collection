44b5cb5698188f496cf580360ebb89855e77f8ef
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-20 15:43:33 -0300

    Variáveis

---
+++ Modulo_1++/variaveis.py
@@ -0,0 +1,46 @@
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+#Lab 1
+
+john = 3
+mary = 5
+adam = 6
+total_apples = john + mary + adam
+
+print("John:", john, ", Mary:", mary,", Adam:", adam)
+print("Total de Maças:" ,total_apples)
+
+#Lab 2
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+print(miles, "milhas é", round(miles_to_kilometers, 2), "quilômetros")
+print(kilometers, "quilômetros é", round(kilometers_to_miles, 2), "milhas")
+
+#Lab 3
+
+x = 0
+x = float(x)
+y = 3 * x **3 - 2 * x ** 2 + 3 * x - 1
+print("y =", y)
+
+#Lab 3
+
+x = 1
+x = float(x)
+y = 3 * x **3 - 2 * x ** 2 + 3 * x - 1
+print("y =", y)
+
+
+#Lab 3
+
+x = -1
+x = float(x)
+y = 3 * x **3 - 2 * x ** 2 + 3 * x - 1
+print("y =", y)
