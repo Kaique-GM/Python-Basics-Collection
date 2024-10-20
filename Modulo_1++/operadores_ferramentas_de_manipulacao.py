b121d80ae806b31b56a7c498dafb914fb8e74858
Kaique-GM <kaiquegmoficial@gmail.com>
2024-10-20 14:30:36 -0300

    Operadores e Ferramentas de Manipulação

---
+++ Modulo_1++/operadores_ferramentas_de_manipulacao.py
@@ -0,0 +1,57 @@
+
+print("Adição")
+print(2+2)
+print(-4 + 4)
+print(-4. + 8)
+
+
+print()
+print("Exponenciação")
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+print(2 ** 2 ** 3)
+
+print()
+print("Multiplicação")
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print()
+print("Divisão")
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+print()
+print("Divisão de número inteiro (divisão arredondada)")
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+print(-6 // 4)
+print(6. // -4)
+
+print()
+print("Restante")
+
+print(14 % 4)
+
+print(12 % 4.5)
+
+print(9 % 6 % 2)
+
+print()
+print("subtração")
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+print()
+print("Operadores e parênteses")
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
